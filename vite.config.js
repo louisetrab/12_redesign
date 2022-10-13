0edec2cacb1be4c6c2881c67a5fecb755049d206
Louise Traberg <louisetrab@gmail.com>
2022-10-13 12:16:50 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,35 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        arkiv: resolve(__dirname, "src/arkiv.html"),
+        cookie: resolve(__dirname, "src/cookie.html"),
+        detlevenderum: resolve(__dirname, "src/det-levende-rum.html"),
+        eventsdramatik: resolve(__dirname, "src/events-dramatik.html"),
+        eventsscenekunst: resolve(__dirname, "src/events-scenekunst.html"),
+        eventstabu: resolve(__dirname, "src/events-tabu.html"),
+        events: resolve(__dirname, "src/events.html"),
+        frygtenforikkeatleve: resolve(
+          __dirname,
+          "src/frygten-for-ikke-at-leve.html"
+        ),
+        gogoyoyoku: resolve(__dirname, "src/go-go-yo-yoku.html"),
+        mandliggersommandharredt: resolve(
+          __dirname,
+          "src/mand-ligger-som-mand-har-redt.html"
+        ),
+        om: resolve(__dirname, "src/om.html"),
+        opencall: resolve(__dirname, "src/opencall.html"),
+        privatliv: resolve(__dirname, "src/privatliv.html"),
+        program: resolve(__dirname, "src/program.html"),
+        rebirth: resolve(__dirname, "src/re-birth.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
+        sitemap: resolve(__dirname, "src/sitemap.xml"),
+        vildmedmig: resolve(__dirname, "src/vild-med-mig.html"),
+        whatfirestogetherwirestogether: resolve(
+          __dirname,
+          "src/what-fires-together-wires-together.html"
+        ),
+
         // about: resolve(__dirname, "src/about.html"),
       },
     },
